4d3d1e43724b188e843f7462e76ff5cb70bde494
Adrian Rydzynski <adrian.rydzynski@allegro.pl>
2019-03-29 08:59:24 +0100

    inicjalizacja zadania z funkcji

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("It works!");
+var suits = ["hearts", "spades", "clubs", "diamonds"];
+var cardNumber = 10;
+var cardObject = { suit: "spades", card: 12 };
+var cardSuit = "clubs";
+var pickRandomCard = function () {
+    return Math.ceil(Math.random() * 13);
+};
+var pickRandomSuit = function () {
+    return Math.ceil(Math.random() * suits.length);
+};
+var displayAllPickedCards = function (array) {
+    array.forEach(function (element) {
+        console.log("card: " + element.card + " of " + element.suit);
+    });
+};
+var pickedCards = [];
+//funkcje nadające wartości pickedCards[0],[1],[2];
+displayAllPickedCards(pickedCards);
--- app.ts
+++ app.ts
@@ -1,1 +1,24 @@
-console.log("It works!");
+const suits = ["hearts", "spades", "clubs", "diamonds"];
+const cardNumber = 10;
+const cardObject = { suit: "spades", card: 12 };
+const cardSuit = "clubs";
+
+const pickRandomCard = () => {
+    return Math.ceil(Math.random() * 13);
+}
+
+const pickRandomSuit = () => {
+    return Math.ceil(Math.random() * suits.length);
+}
+
+const displayAllPickedCards = (array) => {
+    array.forEach(element => {
+        console.log("card: " + element.card + " of " + element.suit);
+    });
+}
+
+let pickedCards = [];
+
+//funkcje nadające wartości pickedCards[0],[1],[2];
+
+displayAllPickedCards(pickedCards);
