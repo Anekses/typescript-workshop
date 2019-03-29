4c2b644392a2643c646dfa01e6d036a55c3116aa
Adrian Rydzynski <adrian.rydzynski@allegro.pl>
2019-03-29 10:01:40 +0100

    rozwiazanie zadania z funkcji

--- app.js
+++ app.js
@@ -3,16 +3,33 @@ var cardNumber = 10;
 var cardObject = { suit: "spades", card: 12 };
 var cardSuit = "clubs";
 var pickRandomCard = function () {
-    return Math.ceil(Math.random() * 13);
+    return Math.ceil(Math.random() * 13 + 1);
 };
 var pickRandomSuit = function () {
-    return Math.ceil(Math.random() * suits.length);
+    return suits[Math.ceil(Math.random() * suits.length - 1)];
 };
 var displayAllPickedCards = function (array) {
     array.forEach(function (element) {
         console.log("card: " + element.card + " of " + element.suit);
     });
 };
+function pickCard(x) {
+    if (typeof x == "string") {
+        var pickedCard = pickRandomCard();
+        var pickedSuit = x;
+        return { suit: pickedSuit, card: pickedCard };
+    }
+    if (typeof x == "object") {
+        return x;
+    }
+    else if (typeof x == "number") {
+        var pickedCard = x % 13;
+        var pickedSuit = pickRandomSuit();
+        return { suit: pickedSuit, card: pickedCard };
+    }
+}
 var pickedCards = [];
-//funkcje nadające wartości pickedCards[0],[1],[2];
+pickedCards.push(pickCard(cardNumber));
+pickedCards.push(pickCard(cardObject));
+pickedCards.push(pickCard(cardSuit));
 displayAllPickedCards(pickedCards);
--- app.ts
+++ app.ts
@@ -3,22 +3,42 @@ const cardNumber = 10;
 const cardObject = { suit: "spades", card: 12 };
 const cardSuit = "clubs";
 
-const pickRandomCard = () => {
-    return Math.ceil(Math.random() * 13);
+const pickRandomCard = (): number => {
+    return Math.ceil(Math.random() * 13 + 1);
 }
 
-const pickRandomSuit = () => {
-    return Math.ceil(Math.random() * suits.length);
+const pickRandomSuit = (): string => {
+    return suits[Math.ceil(Math.random() * suits.length - 1)];
 }
 
-const displayAllPickedCards = (array) => {
+const displayAllPickedCards = (array: {suit: string; card: number; }[]): void => {
     array.forEach(element => {
         console.log("card: " + element.card + " of " + element.suit);
     });
 }
 
-let pickedCards = [];
+function pickCard(x: {suit: string; card: number; }): {suit: string; card: number; };
+function pickCard(x: string): {suit: string; card: number; };
+function pickCard(x: number): {suit: string; card: number; };
+function pickCard(x): any {
+    if (typeof x == "string") {
+        const pickedCard: number = pickRandomCard();
+        const pickedSuit: string = x;
+        return { suit: pickedSuit, card: pickedCard };
+    }
+    if (typeof x == "object") {
+        return x;
+    }
+    else if (typeof x == "number") {
+        const pickedCard: number = x % 13;
+        const pickedSuit: string = pickRandomSuit();
+        return { suit: pickedSuit, card: pickedCard };
+    }
+}
 
-//funkcje nadające wartości pickedCards[0],[1],[2];
+let pickedCards: {suit: string; card: number; }[] = [];
+pickedCards.push(pickCard(cardNumber));
+pickedCards.push(pickCard(cardObject));
+pickedCards.push(pickCard(cardSuit));
 
 displayAllPickedCards(pickedCards);
