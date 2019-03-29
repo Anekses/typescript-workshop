ec915e736edb52115a254e52239c2333ed642913
Adrian Rydzynski <adrian.rydzynski@allegro.pl>
2019-03-29 15:26:39 +0100

    inicjalizacja zadania z klas

--- app.js
+++ app.js
@@ -1,71 +1,25 @@
-// 1. Podstawowa funkcja
-// Dane wejściowe: a, b
-// Dane wyjściowe: suma(a,b)
-var sum = function (a, b) {
-    return a + b;
-};
-console.log("Zadanie 1");
-console.log(sum(2, 5));
-// 2. Parametry opcjonalne
-// Dane wejściowe: a, b, isMultiplication
-// Dane wyjściowe: Jeśli isMultiplication ? mnożenie : dzielenie; Jeśli isMultiplication == false i b == 0, zwróc "b jest równe 0"; 
-var optionalParameters = function (a, b, isMultiplication) {
-    if (isMultiplication) {
-        return a * b;
+var password = "Rocky";
+var Beeing = /** @class */ (function () {
+    function Beeing() {
     }
-    else if (b != 0) {
-        return a / b;
+    return Beeing;
+}());
+var Animal = /** @class */ (function () {
+    function Animal() {
     }
-    else {
-        return "b jest równe 0";
+    Animal.prototype.move = function (distance) {
+        console.log("The " + name + " moved " + distance + " meters");
+    };
+    return Animal;
+}());
+var Dog = /** @class */ (function () {
+    function Dog() {
     }
-};
-console.log("Zadanie 2");
-console.log(optionalParameters(5, 10, true));
-console.log(optionalParameters(5, 10, false));
-console.log(optionalParameters(5, 10));
-console.log(optionalParameters(5, 0));
-// 3. Domyślne wartości
-// Dane wejściowe: a, b, Enum operacji matematycznych - domyślnie suma
-// Dane wyjściowe: suma, różnica, mnożenie, dzielenie. Jeśli b == 0, od razu return 0;
-var MathOperations;
-(function (MathOperations) {
-    MathOperations[MathOperations["Sum"] = 0] = "Sum";
-    MathOperations[MathOperations["Diff"] = 1] = "Diff";
-    MathOperations[MathOperations["Multi"] = 2] = "Multi";
-    MathOperations[MathOperations["Div"] = 3] = "Div";
-})(MathOperations || (MathOperations = {}));
-;
-var defaultParameters = function (a, b, operation) {
-    if (operation === void 0) { operation = MathOperations.Sum; }
-    if (b == 0)
-        return 0;
-    switch (operation) {
-        case MathOperations.Sum:
-            return a + b;
-        case MathOperations.Diff:
-            return a - b;
-        case MathOperations.Multi:
-            return a * b;
-        case MathOperations.Div:
-            return a / b;
-        default:
-            return 0;
-    }
-};
-console.log("Zadanie 3");
-console.log(defaultParameters(5, 10, MathOperations.Sum));
-console.log(defaultParameters(5, 10));
-console.log(defaultParameters(5, 10, MathOperations.Multi));
-// 4. Rest parameters
-// Dane wejściowe: cztery stringi "Joseph", "Samuel", "Lucas", "MacKinzie"
-// Dane wyjściowe: konkatenacja stringów (ze spacją pomiędzy), gdzie pierwszy z nich jest użyty dwukrotnie (na początku i na końcu)
-var restParameters = function (firstName) {
-    var restOfNames = [];
-    for (var _i = 1; _i < arguments.length; _i++) {
-        restOfNames[_i - 1] = arguments[_i];
-    }
-    return firstName + " " + restOfNames.join(" ") + " " + firstName;
-};
-console.log("Zadanie 4");
-console.log(restParameters("Joseph", "Samuel", "Lucas", "MacKinzie"));
+    Dog.prototype.move = function (distance) {
+        console.log("The " + name + " moved " + distance + " meters");
+    };
+    Dog.prototype.bark = function () {
+        console.log('Woof!');
+    };
+    return Dog;
+}());
--- app.ts
+++ app.ts
@@ -1,60 +1,33 @@
-// 1. Podstawowa funkcja
-// Dane wejściowe: a, b
-// Dane wyjściowe: suma(a,b)
-const sum = (a: number, b: number): number => {
-    return a + b;
-}
-console.log("Zadanie 1");
-console.log(sum(2,5));
+let password = "Rocky";
 
-// 2. Parametry opcjonalne
-// Dane wejściowe: a, b, isMultiplication
-// Dane wyjściowe: Jeśli isMultiplication ? mnożenie : dzielenie; Jeśli isMultiplication == false i b == 0, zwróc "b jest równe 0"; 
-const optionalParameters = (a: number, b: number, isMultiplication?: boolean): number | string => {
-    if (isMultiplication) {
-        return a * b;
-    } else if (b != 0) {
-        return a / b;
-    } else {
-        return "b jest równe 0";
+abstract class Beeing {
+    abstract age;
+    name;
+    numberOfLegs;
+
+    abstract grow();
+}
+
+class Animal {
+    age;
+    name;
+
+    move(distance) {
+        console.log(`The ${name} moved ${distance} meters`);
     }
 }
-console.log("Zadanie 2");
-console.log(optionalParameters(5, 10, true));
-console.log(optionalParameters(5, 10, false));
-console.log(optionalParameters(5, 10));
-console.log(optionalParameters(5, 0));
 
-// 3. Domyślne wartości
-// Dane wejściowe: a, b, Enum operacji matematycznych - domyślnie suma
-// Dane wyjściowe: suma, różnica, mnożenie, dzielenie. Jeśli b == 0, od razu return 0;
-enum MathOperations { Sum, Diff, Multi, Div};
-const defaultParameters = (a: number, b: number, operation: MathOperations = MathOperations.Sum): number => {
-    if (b == 0) return 0;
-    switch(operation) {
-        case MathOperations.Sum:
-            return a + b;
-        case MathOperations.Diff:
-            return a - b;
-        case MathOperations.Multi:
-            return a * b;
-        case MathOperations.Div:
-            return a / b;
-        default:
-            return 0;
+class Dog {
+    age;
+    name;
+    color;
+    hasChildren;
+
+    move(distance) {
+        console.log(`The ${name} moved ${distance} meters`);
+    }
+
+    bark() {
+        console.log('Woof!');
     }
 }
-console.log("Zadanie 3");
-console.log(defaultParameters(5, 10, MathOperations.Sum));
-console.log(defaultParameters(5, 10));
-console.log(defaultParameters(5, 10, MathOperations.Multi));
-
-// 4. Rest parameters
-// Dane wejściowe: cztery stringi "Joseph", "Samuel", "Lucas", "MacKinzie"
-// Dane wyjściowe: konkatenacja stringów (ze spacją pomiędzy), gdzie pierwszy z nich jest użyty dwukrotnie (na początku i na końcu)
-const restParameters = (firstName: string, ...restOfNames: string[]): string => {
-    return firstName + " " + restOfNames.join(" ") + " " + firstName;
-}
-
-console.log("Zadanie 4");
-console.log(restParameters("Joseph", "Samuel", "Lucas", "MacKinzie"));
