ac008125101466a2c475b45d6898bacd9b07a851
Anekses <adrian.rydzynski@gmail.com>
2019-03-31 21:47:29 +0200

    rozwiazanie zadania z interfejsow

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 //Zadania 1-2
+// interface NamedPerson {
+//     firstName: string,
+//     age?: number,
+//     [propName: string]: any,
+//     greet?(lastName: string): void
+// }
 var greet = function (person) {
     console.log("Hello, " + person.firstName);
 };
@@ -30,13 +36,30 @@ var person3 = {
 greet(person0);
 changeName(person0, "Johny");
 greet(person0);
-// greet(person1);
-// greet(person2);
-// greet(person3);
-//Zadanie 3
-var isThisSamePersonFunction = function (p1, p2) {
+greet(person1);
+greet(person2);
+greet(person3);
+var isThisSamePersonFunction;
+isThisSamePersonFunction = function (p1, p2) {
     if (p1.firstName == p2.firstName) {
         return true;
     }
     return false;
+};
+//Zadanie 4
+var Person = /** @class */ (function () {
+    function Person(firstName, age) {
+        this.firstName = firstName;
+        this.age = age;
+    }
+    Person.prototype.greet = function (lastName) {
+        console.log("Hi, I am " + this.firstName + " " + lastName);
+    };
+    return Person;
+}());
+var myPerson = new Person("Stefan", 40);
+myPerson.greet("Stefański");
+var person4 = {
+    firstName: "Jon",
+    hobbies: ["Cooking", "TV"]
 };
--- app.ts
+++ app.ts
@@ -1,4 +1,11 @@
 //Zadania 1-2
+// interface NamedPerson {
+//     firstName: string,
+//     age?: number,
+//     [propName: string]: any,
+//     greet?(lastName: string): void
+// }
+
 let greet = (person) => {
     console.log(`Hello, ${person.firstName}`);
 }
@@ -7,23 +14,23 @@ let changeName = (person, newName) => {
     person.firstName = newName;
 }
 
-let person0 = {
+let person0: NamedPerson = {
     firstName: "Jon"
 }
 
-let person1 = {
+let person1: NamedPerson = {
     firstName: "Jon",
     age: 30
 }
 
-let person2 = {
+let person2: NamedPerson = {
     firstName: "Jon",
     age: 30,
     hobbies: ["Cooking", "TV"],
     car: "Mazda"
 }
 
-let person3 = {
+let person3: NamedPerson = {
     firstName: "Jon",
     age: 30,
     hobbies: ["Cooking", "TV"],
@@ -36,14 +43,66 @@ let person3 = {
 greet(person0);
 changeName(person0, "Johny");
 greet(person0);
-// greet(person1);
-// greet(person2);
-// greet(person3);
+greet(person1);
+greet(person2);
+greet(person3);
 
 //Zadanie 3
-let isThisSamePersonFunction = (p1, p2) => {
+interface isThisSamePerson {
+    (firstPerson: NamedPerson, secondPerson: NamedPerson): boolean;
+}
+
+let isThisSamePersonFunction: isThisSamePerson;
+isThisSamePersonFunction = (p1: NamedPerson, p2: NamedPerson) => {
     if (p1.firstName == p2.firstName) {
         return true;
     }
     return false;
 }
+
+//Zadanie 4
+class Person implements NamedPerson {  
+    firstName: string;
+    age: number;
+    hobbies: string[];
+
+    constructor(firstName: string, age: number) {
+        this.firstName = firstName;
+        this.age = age;
+    }
+
+    greet(lastName: string): void {
+        console.log(`Hi, I am ${this.firstName} ${lastName}`)
+    }
+}
+
+let myPerson: Person = new Person("Stefan", 40);
+myPerson.greet("Stefański");
+
+//Zadanie 5
+interface NamedPerson {
+    hobbies?: string[];
+}
+
+let person4: NamedPerson = {
+    firstName: "Jon",
+    hobbies: ["Cooking", "TV"]
+}
+
+//Zadanie 6
+interface NamedPerson {
+    firstName: string
+}
+
+interface NamedPerson {
+    age?: number,
+}
+
+interface NamedPerson {
+    [propName: string]: any,
+}
+
+interface NamedPerson {
+    greet?(lastName: string): void
+}
+
