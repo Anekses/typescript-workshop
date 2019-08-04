82877032458a375be573991fa89e1e32a52137e3
Anekses <adrian.rydzynski@gmail.com>
2019-08-04 22:40:29 +0200

    remove js files

--- src/task4/task4.js
+++
@@ -1,7 +0,0 @@
-// 4. Rest parameters
-//     Dane wejściowe: cztery stringi "Joseph", "Samuel", "Lucas", "MacKinzie"
-//     Dane wyjściowe: konkatenacja stringów (ze spacją pomiędzy), gdzie pierwszy z nich jest użyty dwukrotnie (na początku i na końcu)
-const restParameters = (firstName, ...restOfNames) => {
-    return firstName + " " + restOfNames.join(" ") + " " + firstName;
-};
-module.exports = restParameters;
