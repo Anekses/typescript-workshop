ac63e40ba15b68100de3feb4d624ad8525431bb8
Adrian Mróź <adrianmroz@users.noreply.github.com>
2019-08-06 11:05:41 +0200

    generics solutions

--- src/generics/index.ts
+++ src/generics/index.ts
@@ -11,7 +11,7 @@ function failedId2(a: unknown): unknown {
 }
 
 // TODO
-function id(a) {
+function id<T>(a: T): T {
   return a;
 }
 
@@ -20,7 +20,7 @@ id(123).toFixed();
 id({ a: 1 }).a;
 
 // TODO
-function map(arr, f) {
+function map<T, U>(arr: T[], f: (e: T) => U): U[] {
   return arr.map(f);
 }
 
@@ -34,33 +34,55 @@ function getPrice<T extends WithPrice>(o: T): number {
 
 getPrice({ name: 'foobar', price: 100 })
 
+
 // TODO
-function doublePrice(o) {
-  // returns object with price doubled
+function doublePrice<T extends WithPrice>(o: T): T {
+  return { ...o, price: o.price * 2 };
 }
 
 // CHECK
 doublePrice({ name: 'oil', price: 100 }).name;
 
-// TODO
-class Stringifier {
-  
+
+class Stringifier<T> {
+  constructor(private val: T) {
+  }
+
+  getValue(): T {
+    return this.val;
+  }
+
+  getString(): string {
+    return String(this.val);
+  }
 }
 
 const a = new Stringifier(123);
 a.getValue().toFixed();
 a.getString().toLowerCase();
 
-// TODO
-class Stack {
-  
+class Stack<T> {
+  private values: Array<T> = [];
+
+  // TODO
+  push(e: T): void {
+    this.values.push(e);
+  }
+  // TODO
+  pop(): T | undefined {
+    return this.values.pop();
+  }
+  // TODO
+  toArray(): Array<T> {
+    return this.values;
+  }
 }
 
-// CHECK
-const stack = new Stack();
+const stack = new Stack<number>();
 stack.push(123);
 stack.toArray()[0].toFixed();
-stack.pop().toFixed();
+const elem = stack.pop();
+elem && elem.toFixed();
 
 
 function pick<T, K extends keyof T>(o: T, key: K): T[K] {
@@ -68,12 +90,9 @@ function pick<T, K extends keyof T>(o: T, key: K): T[K] {
 }
 
 // TODO
-function entry(o, key) {
-  // returns tuple of key and value
+function entry<T, K extends keyof T>(o: T, key: K): [K, T[K]] {
+  return [key, o[key]];
 }
-
-// CHECK
-
 
 const obj = {
   price: 123,
@@ -83,3 +102,19 @@ const obj = {
 const [key, value] = entry(obj, 'price');
 key.toLowerCase();
 value.toFixed();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
